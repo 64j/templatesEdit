1965a31b44bc95f04123250acae0fd172ed22ec9
64j <64j@mail.ru>
2014-07-08 11:34:56 +0400

    Create plugin.templatesedit.tpl.php

---
+++ plugin/plugin.templatesedit.tpl.php
@@ -0,0 +1,8 @@
+//<?php
+/* templatesEdit
+* plugin
+* @events OnDocFormTemplateRender, OnTVFormDelete, OnTVFormSave, OnTempFormDelete, OnTempFormSave
+* @settings &editableTemplate=Редактировать шаблоны;list;true,false;true &altRenderTemplate=Альтернативный вывод шаблона (div);list;true,false;false
+*/
+
+require_once MODX_BASE_PATH."assets/modules/templatesEdit/plugin/plugin.templatesedit.php";
