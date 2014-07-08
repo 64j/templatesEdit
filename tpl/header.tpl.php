c9599a19762980e8ef552eab771f30eebe12e265
64j <64j@mail.ru>
2014-07-08 11:35:54 +0400

    Create header.tpl.php

---
+++ tpl/header.tpl.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"  lang="en" xml:lang="en">
+<head>
+<link rel="stylesheet" type="text/css" href="media/style/<?php echo $theme; ?>/style.css" />
+</head>
+<body>
+<h1>Управление шаблонами</h1>
+<div class="sectionBody" style="min-height:250px;">
+<div class="clear"></div>
